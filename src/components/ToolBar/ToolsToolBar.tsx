35e7c77cd54e10520a7e866afe8e9b24b6d50351
nitesh-dev <niteshdev547@gmail.com>
2023-12-21 11:30:09 +0530

    tools toolbar done

---
+++ src/components/ToolBar/ToolsToolBar.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+export function ToolsToolBar() {
+  return (
+    <div className="tools">
+      <button>Horizontal Line</button>
+      <button>Vertical Line</button>
+      <button>Ray</button>
+      <button>Fibonacci Retracement</button>
+      <button>Fibonacci Fan</button>
+      <button>Trend Line</button>
+      <button>Parallel Channel</button>
+      <button>Rectangle</button>
+      <button>Andrew's Pitchfork</button>
+    </div>
+  );
+}
--- src/components/ToolBar/index.tsx
+++ src/components/ToolBar/index.tsx
@@ -4,6 +4,7 @@ import "../../styles/toolbar/common.css";
 import { AssetsToolBar } from "./AssetsToolBar";
 import { ChartToolBar } from "./ChartToolBar";
 import { IndicatorToolBar } from "./IndicatorToolBar";
+import { ToolsToolBar } from "./ToolsToolBar";
 
 enum ToolVisible {
   none,
@@ -92,7 +93,14 @@ export function ToolBar() {
         </div>
 
         <div>
-          <button className="inner-btn">
+          <button
+            className={
+              toolVisible == ToolVisible.tools
+                ? "inner-btn active"
+                : "inner-btn"
+            }
+            onClick={() => toggleDropdown(ToolVisible.tools)}
+          >
             <svg
               fill="none"
               viewBox="0 0 24 24"
@@ -101,6 +109,7 @@ export function ToolBar() {
               <path d="M13.94 5 19 10.06 9.062 20a2.25 2.25 0 0 1-.999.58l-5.116 1.395a.75.75 0 0 1-.92-.921l1.395-5.116a2.25 2.25 0 0 1 .58-.999L13.938 5Zm7.09-2.03a3.578 3.578 0 0 1 0 5.06l-.97.97L15 3.94l.97-.97a3.578 3.578 0 0 1 5.06 0Z" />
             </svg>
           </button>
+          {toolVisible == ToolVisible.tools && <ToolsToolBar />}
         </div>
       </div>
       <p className="legend">12:08:17 UTC+5:30</p>
