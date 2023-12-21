d30f69fee66b27248472c96d6ed6ad71814cee66
nitesh-dev <niteshdev547@gmail.com>
2023-12-21 11:09:53 +0530

    indicator panel done

---
+++ src/components/ToolBar/IndicatorToolBar.tsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import { IconStar } from "../icons/IconStar";
+
+export function IndicatorToolBar() {
+  return (
+    <div className="indicators">
+      <button>
+        <IconStar />
+        Accelerator Oscillator
+      </button>
+      <button>
+        <IconStar />
+        ADX
+      </button>
+      <button>
+        <IconStar />
+        Alligator
+      </button>
+      <button>
+        <IconStar />
+        Aroon
+      </button>
+      <button>
+        <IconStar />
+        Average true range
+      </button>
+      <button>
+        <IconStar />
+        Awesome Oscillator
+      </button>
+      <button>
+        <IconStar />
+        Bears power
+      </button>
+      <button>
+        <IconStar />
+        Bollinger Bands
+      </button>
+      <button>
+        <IconStar />
+        Bollinger Bands Width
+      </button>
+      <button>
+        <IconStar />
+        Bulls power
+      </button>
+      <button>
+        <IconStar />
+        CCI
+      </button>
+      <button>
+        <IconStar />
+        Donchian Channels
+      </button>
+      <button>
+        <IconStar />
+        BeMarker
+      </button>
+      <button>
+        <IconStar />
+        Envelopes
+      </button>
+      <button>
+        <IconStar />
+        Fractal
+      </button>
+      <button>
+        <IconStar />
+        Fractal Chaos Bands
+      </button>
+      <button>
+        <IconStar />
+        Ichimoky Kinko Hyo
+      </button>
+      <button>
+        <IconStar />
+        Keltner channel
+      </button>
+      <button>
+        <IconStar />
+        MACD
+      </button>
+      <button>
+        <IconStar />
+        Momentum
+      </button>
+      <button>
+        <IconStar />
+        Moving Average
+      </button>
+      <button>
+        <IconStar />
+        OsMA
+      </button>
+      <button>
+        <IconStar />
+        Parabolic SAR
+      </button>
+      <button>
+        <IconStar />
+        RSI
+      </button>
+      <button>
+        <IconStar />
+        Rate Of Change
+      </button>
+      <button>
+        <IconStar />
+        Schaff Trend Cycle
+      </button>
+      <button>
+        <IconStar />
+        Stochastic Oscillator
+      </button>
+      <button>
+        <IconStar />
+        SuperTrend
+      </button>
+      <button>
+        <IconStar />
+        Vortex
+      </button>
+      <button>
+        <IconStar />
+        Williams %R
+      </button>
+      <button>
+        <IconStar />
+        Zig Zag
+      </button>
+    </div>
+  );
+}
--- src/components/ToolBar/index.tsx
+++ src/components/ToolBar/index.tsx
@@ -3,6 +3,7 @@ import "../../styles/toolbar/index.css";
 import "../../styles/toolbar/common.css";
 import { AssetsToolBar } from "./AssetsToolBar";
 import { ChartToolBar } from "./ChartToolBar";
+import { IndicatorToolBar } from "./IndicatorToolBar";
 
 enum ToolVisible {
   none,
@@ -71,7 +72,14 @@ export function ToolBar() {
         </div>
 
         <div>
-          <button className="inner-btn">
+          <button
+            className={
+              toolVisible == ToolVisible.indicator
+                ? "inner-btn active"
+                : "inner-btn"
+            }
+            onClick={() => toggleDropdown(ToolVisible.indicator)}
+          >
             <svg
               fill="none"
               viewBox="0 0 24 24"
@@ -80,6 +88,7 @@ export function ToolBar() {
               <path d="M8.75 13.5a3.251 3.251 0 0 1 3.163 2.5h9.337a.75.75 0 0 1 .102 1.493l-.102.007h-9.337a3.251 3.251 0 0 1-6.326 0H2.75a.75.75 0 0 1-.102-1.493L2.75 16h2.837a3.251 3.251 0 0 1 3.163-2.5Zm6.5-9.5a3.251 3.251 0 0 1 3.163 2.5h2.837a.75.75 0 0 1 .102 1.493L21.25 8h-2.837a3.251 3.251 0 0 1-6.326 0H2.75a.75.75 0 0 1-.102-1.493L2.75 6.5h9.337A3.251 3.251 0 0 1 15.25 4Z" />
             </svg>
           </button>
+          {toolVisible == ToolVisible.indicator && <IndicatorToolBar />}
         </div>
 
         <div>
