838c6faca9f96ad1adcf6e9e916aa24e0724a857
nitesh-dev <niteshdev547@gmail.com>
2024-01-08 14:07:26 +0530

    data feed added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback, useEffect, useRef } from "react";
+import { createContext, useCallback, useEffect, useRef, useState } from "react";
 import { ToolBar } from "./components/ToolBar";
 // import { createChart, generateCandlesData } from "@devexperts/dxcharts-lite";
 import { RightPanel } from "./components/RightPanel";
@@ -13,7 +13,7 @@ import {
 import { ChartReactAPI } from "@dx-private/dxchart5-react/dist/chart/view-models/api/chart-react-api.view-model";
 import { ChartAreaTheme } from "@devexperts/dxcharts-lite/dist/chart/chart.config";
 import { TradeObjectDrawer } from "./plugins/TradeObjectDrawer";
-import { Chart } from "@dx-private/dxchart5-modules";
+import { Chart, generateCandlesData } from "@dx-private/dxchart5-modules";
 import { ChartWithModules } from "@dx-private/dxchart5-react/dist/chart/components/canvas-chart-renderer/chart-with-modules";
 import { CenterHoverDrawer } from "./plugins/CenterHoverDrawer";
 import { ToastProvider } from "./components/widgets/Toast";
@@ -26,6 +26,43 @@ import json1 from "./others/data1.json";
 import json2 from "./others/data2.json";
 import { io, Socket } from "socket.io-client";
 
+import {
+  ChartCandleData,
+  ChartDataOptions,
+  ChartDataProvider,
+  ServiceData,
+} from "@dx-private/dxchart5-react/dist/providers/chart-data-provider";
+import { AggregationPeriod } from "@dx-private/dxchart5-react/dist/chart/model/aggregation.model";
+
+async function genData(): Promise<ChartCandleData[]> {
+  // const data = generateCandlesData({ quantity: 5 });
+
+  const data = json1.map((item: any) => {
+    return {
+      hi: item.high,
+      lo: item.low,
+      open: item.open,
+      close: item.close,
+      timestamp: item.time * 1000,
+      volume: 0,
+      isVisible: true,
+    };
+  });
+
+  const newData: ChartCandleData[] = data.map((item) => {
+    return {
+      close: item.close,
+      high: item.hi,
+      low: item.lo,
+      open: item.open,
+      time: item.timestamp,
+      volume: item.volume,
+    } as ChartCandleData;
+  });
+  console.log("gen data");
+  return newData;
+}
+
 function App() {
   const chartReactAPI = useRef<ChartReactAPI>();
   const chartsRef = useRef<ChartWithModules[]>([]);
@@ -33,6 +70,47 @@ function App() {
   const socketRef = useRef<Socket>();
   const timeFrameIntervalSecRef = useRef<number>(10);
   const lastCandleTimestamp = useRef<number>(0);
+  const [dataProvider, setDataProvider] = useState<ChartDataProvider>();
+
+  const defaultData = useRef<ChartCandleData[]>([]);
+
+  async function genData2() {
+    return defaultData.current;
+  }
+
+  useEffect(() => {
+    (async () => {
+      defaultData.current = await genData();
+
+      setDataProvider({
+        requestHistoryData(
+          symbol: string,
+          aggregation: AggregationPeriod,
+          options?: {
+            fromTime?: number;
+            toTime?: number;
+          } & ChartDataOptions
+        ): Promise<ChartCandleData[]> {
+          return genData2();
+        },
+        subscribeCandles(
+          symbol: string,
+          aggregation: AggregationPeriod,
+          subscriptionId: string,
+          subscribeCallback: (data: ChartCandleData[]) => void,
+          options?: ChartDataOptions
+        ): void {
+
+
+        },
+        unsubscribeCandles(subscriptionId: string): void {},
+        subscribeServiceData(
+          symbol: string,
+          subscribeCallback: (data: ServiceData) => void
+        ): void {},
+      });
+    })();
+  }, []);
 
   const onChartCreated = useCallback((chart: Chart) => {
     const hoverDrawer = new CenterHoverDrawer(chart);
@@ -63,15 +141,15 @@ function App() {
 
     // } , 1000)
 
-    // const socket = io("http://85.206.172.238:2088", {
-    //   reconnectionDelayMax: 10000,
-    // });
+    const socket = io("http://85.206.172.238:2088", {
+      reconnectionDelayMax: 10000,
+    });
 
-    // socket.on("connect", () => {
-    //   console.log("connected");
-    // })
+    socket.on("connect", () => {
+      console.log("connected");
+    })
 
-    // socket.emit("room", "BTCUSD");
+    socket.emit("room", "BTCUSD");
     // socket.on(
     //   "notification",
     //   function (data: any) {
@@ -98,38 +176,96 @@ function App() {
     //   }
     // );
 
-    // socket.on(
-    //   "notification",
-    //   function (data: {
-    //     SymbolID: number;
-    //     Symbol: string;
-    //     Bid: number;
-    //     Ask: number;
-    //     High: number;
-    //     Low: number;
-    //     Close: number;
-    //     Time: string;
-    //   }) {
-    //     console.log(data);
-    //     // const _candles = generateCandlesData({ quantity: 1 });
-    //     const candle = {
-    //       hi: data.High ,
-    //       lo: data.Low ,
-    //       open: data.Bid ,
-    //       close: data.Bid ,
-    //       timestamp: Date.now(),
-    //       volume: 0,
-    //       isVisible: true,
-    //     };
+    socket.on(
+      "notification",
+      function (data: {
+        SymbolID: number;
+        Symbol: string;
+        Bid: number;
+        Ask: number;
+        High: number;
+        Low: number;
+        Close: number;
+        Time: string;
+      }) {
+        console.log(data);
+        // const _candles = generateCandlesData({ quantity: 1 });
+        // const candle = {
+        //   hi: data.Bid ,
+        //   lo: data.Bid ,
+        //   open: data.Bid ,
+        //   close: data.Bid ,
+        //   timestamp: Date.now(),
+        //   volume: 0,
+        //   isVisible: true,
+        // };
 
-    //     chart.data.addLastCandle(candle);
-    //     console.log(chartsRef.current.length, "length");
-    //     chart.redraw();
-    //     chart.drawingManager.forceDraw();
-    //     chart.scale.autoScale(true);
-    //     chart.timeZoneModel.observeTimeZoneChanged();
-    //   }
-    // );
+         console.log(chart);
+        const a = chart.chartBaseModel.mainDataPoints
+
+        var lastDataObject = a[a.length - 1];
+        if(lastDataObject)
+        {
+          console.log(a[a.length - 1])
+          var previousDate = lastDataObject.timestamp;
+          var high = lastDataObject.hi;
+          var low = lastDataObject.lo;
+          var open = lastDataObject.open;
+          var m1 = 60; //Current durationType (s -1, m - 60, h - 3600, d - 86400)
+          var m2 = 1; // Current Duration
+  
+
+          var nextChange = previousDate + (m1 * m2 * 1000);
+          var ts = Math.round((new Date()).getTime());
+
+          if (ts >= nextChange) {
+
+            const candleAdd = {
+              hi: data.Bid ,
+              lo: data.Bid ,
+              open: data.Bid ,
+              close: data.Bid ,
+              timestamp: Date.now(),
+              volume: 0,
+              isVisible: true,
+            };
+      
+            chart.data.addLastCandle(candleAdd);
+
+            // sbSeries.data.push(dObj1);
+            previousDate = candleAdd.timestamp;
+        } else
+        {
+
+          const candleUpdate = {
+            hi: data.Bid > high ? data.Bid : high,
+            lo: data.Bid < low ? data.Bid : low  ,
+            open: open,
+            close: data.Bid ,
+            timestamp: previousDate,
+            volume: 0,
+            isVisible: true,
+          };
+    
+          chart.data.updateLastCandle(candleUpdate);
+
+        }
+  
+  
+  
+
+        }
+
+
+
+        // chart.data.addLastCandle(candle);
+        // console.log(chartsRef.current.length, "length");
+        // chart.redraw();
+        // chart.drawingManager.forceDraw();
+        // chart.scale.autoScale(true);
+        // chart.timeZoneModel.observeTimeZoneChanged();
+      }
+    );
 
     // setInterval(() => {
     //   if (isFirst) {
@@ -185,13 +321,12 @@ function App() {
 
     // chart.setData({ candles: _candles });
     // chart.data.setMainSeries({ candles: _candles });
-
   }, []);
 
   const onApiCreated = useCallback((api: ChartReactAPI) => {
     chartReactAPI.current = api;
     chartReactAPI.current.internal.multiChartViewModel.setChartTypeSync(true);
-    chartReactAPI.current.internal.multiChartViewModel.setChartType("area");
+    // chartReactAPI.current.internal.multiChartViewModel.setChartType("area");
 
     // const a: TStudySettings[] = [{
     //   id: "sdfd",
@@ -274,25 +409,25 @@ function App() {
           isVisible: true,
         };
 
-        if (chartRef.current) {
-          if (
-            candle.timestamp - lastCandleTimestamp.current >
-            timeFrameIntervalSecRef.current * 1000
-          ) {
-            lastCandleTimestamp.current = candle.timestamp;
-            chartRef.current.data.addLastCandle(candle);
-            console.log("added");
-          } else {
-            candle.timestamp = lastCandleTimestamp.current;
-            chartRef.current.data.updateLastCandle(candle);
-            console.log("updated");
-          }
+        // if (chartRef.current) {
+        //   if (
+        //     candle.timestamp - lastCandleTimestamp.current >
+        //     timeFrameIntervalSecRef.current * 1000
+        //   ) {
+        //     lastCandleTimestamp.current = candle.timestamp;
+        //     chartRef.current.data.addLastCandle(candle);
+        //     console.log("added");
+        //   } else {
+        //     candle.timestamp = lastCandleTimestamp.current;
+        //     chartRef.current.data.updateLastCandle(candle);
+        //     console.log("updated");
+        //   }
 
-          chartRef.current.redraw();
-          chartRef.current.drawingManager.forceDraw();
-          chartRef.current.scale.autoScale(true);
-          chartRef.current.timeZoneModel.observeTimeZoneChanged();
-        }
+        //   chartRef.current.redraw();
+        //   chartRef.current.drawingManager.forceDraw();
+        //   chartRef.current.scale.autoScale(true);
+        //   chartRef.current.timeZoneModel.observeTimeZoneChanged();
+        // }
       }
     );
   }, []);
@@ -308,6 +443,7 @@ function App() {
   function setLastCandleTimestamp(timestamp: number) {
     console.log(timestamp, "timestamp");
   }
+
   function changeTimeFrameInterval(time: number) {
     timeFrameIntervalSecRef.current = time;
   }
@@ -327,16 +463,32 @@ function App() {
             >
               <ChartReactApp
                 dependencies={{
+                  chartDataProvider: dataProvider,
+                  initialChartConfig: {
+                    scale: {
+                      auto: true,
+                    },
+                  },
+
                   onApiCreated,
                   localization: {
                     studies: {},
                   },
+                  initialInstrument: "BTCUSD",
+                  initialAggregationPeriods: [
+                    { duration: 30, durationType: "m" },
+                    { duration: 1, durationType: "h" },
+                  ],
+                  initialAggregation: {
+                    duration: 1,
+                    durationType: "s",
+                  },
                   initialChartReactSettings: {
                     legend: {
-                      showOHLC: false,
+                      showOHLC: true,
                       showVolume: false,
-                      showInstrument: false,
-                      showPeriod: false,
+                      showInstrument: true,
+                      showPeriod: true,
                     },
                   },
 
@@ -348,7 +500,7 @@ function App() {
                     },
                     toolbar: {
                       showButtonsTooltip: true,
-                      enabled: false,
+                      enabled: true,
                     },
                   },
                 }}
